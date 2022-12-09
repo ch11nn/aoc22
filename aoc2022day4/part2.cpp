c79b713249ea8435f48e8fd42606743068e05fed
ch11nn <119714575+ch11nn@users.noreply.github.com>
2022-12-09 21:55:39 +0800

    Add files via upload

---
+++ aoc2022day4/part2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  string range;
+  int RepeatedPair = 0;
+
+  ifstream pairs("input.txt");
+  while (getline(pairs,range)) { 
+  string first = range.substr(0,range.find(","));
+  string second = range.substr(range.find(",")+1);
+
+  int lower1 = stoi(first.substr(0,first.find("-")));
+  int upper1 = stoi(first.substr(first.find("-")+1));
+
+  int lower2 = stoi(second.substr(0,second.find("-")));
+  int upper2 = stoi(second.substr(second.find("-")+1));
+
+  if (lower1 >= lower2 and lower1 <= upper2)
+        RepeatedPair += 1;
+  else if (upper1 >= lower2 and upper1 <= upper2)
+        RepeatedPair += 1;
+  else if (lower2 >= lower1 and lower2 <= upper1)
+        RepeatedPair += 1;
+  else if (upper2 >= lower1 and upper2 <= upper1)
+        RepeatedPair += 1;
+  }
+  cout<<RepeatedPair<<"\n";
+  pairs.close();
+  return 0;
+}
